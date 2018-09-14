784f2235efa24c10b84ea853796ff5a07247dc12
lolita89 <cika.lolita@hotmail.com>
2018-09-14 14:07:53 +0200

    some changes after Andrii suggestions

--- hangman.js
+++ hangman.js
@@ -12,43 +12,48 @@ let letter = letters.split("");
 function assign(valueLetter) {
     console.log(valueLetter);
     var letter = valueLetter;
+
+    var statementContainer = document.getElementById('statement');
+    var htmlCorrectLetter = '<p>Correct letter!</p>';
+    var htmlWrongLetter = '<p>Wrong letter!</p>';
+    var getKeyboardId = document.getElementById('keyboard');
     
     if (letter === "g") {
-        document.getElementById('statement').innerHTML = '<p>Correct letter!</p>';
+        statementContainer.innerHTML = htmlCorrectLetter;
         document.getElementById('0').value = "g";
         rightGuesses++;
         // console.log(rightGuesses);
     } else if (letter === "o"){
-        document.getElementById('statement').innerHTML = '<p>Correct letter!</p>';
+        statementContainer.innerHTML = htmlCorrectLetter;
         document.getElementById('1').value = "o";
         rightGuesses++;
         // console.log(rightGuesses);
     } else if (letter === "a"){
-        document.getElementById('statement').innerHTML = '<p>Correct letter!</p>';
+        statementContainer.innerHTML = htmlCorrectLetter;
         document.getElementById('2').value = "a";
         rightGuesses++;
         // console.log(rightGuesses);
     } else if (letter === "l"){
-        document.getElementById('statement').innerHTML = '<p>Correct letter!</p>';
+        statementContainer.innerHTML = htmlCorrectLetter;
         document.getElementById('3').value = "l";
         rightGuesses = rightGuesses + 1;
     } else {
         wrongGuesses = wrongGuesses + 1;
-        document.getElementById('statement').innerHTML = '<p>Wrong letter!</p>';
+        statementContainer.innerHTML = htmlWrongLetter;
         // console.log(wrongGuesses);
         document.getElementById("timeLeft").stepDown(1);
     }
     
     //if user guest all correct letters
-    if (rightGuesses === 4){
-        document.getElementById('statement').innerHTML = '<p>Congrats you won! :D</p>';
-        document.getElementById('keyboard').innerHTML = `<img src="https://media.giphy.com/media/4AZ7jvyD54AFO/giphy.gif" />`
+    if (rightGuesses === word.length){
+        statementContainer.innerHTML = '<p>Congrats you won! :D</p>';
+        getKeyboardId.innerHTML = `<img src="https://media.giphy.com/media/4AZ7jvyD54AFO/giphy.gif" />`
     }
 
     // if user have 8 times wrong
     if (wrongGuesses === 8){
-        document.getElementById('statement').innerHTML = '<p>Game over!</p>';
-        document.getElementById('keyboard').innerHTML = `<img src="https://media.giphy.com/media/132bzOAtymSqc0/giphy.gif" />`
+        statementContainer.innerHTML = '<p>Game over!</p>';
+        getKeyboardId.innerHTML = `<img src="https://media.giphy.com/media/132bzOAtymSqc0/giphy.gif" />`
     }
 
     //everytime it is clicked, the keyboard will be changed to "keyboard-after"
@@ -67,7 +72,7 @@ var text = "";
 for ( var i = 0; i < letter.length; i++) {
         text += letter[i];
         document.getElementById('keyboard').innerHTML += '<button onclick="assign(this.id)" id="'+letter[i]+'" class="keyboard">' + letter[i] + '</button>';
-        // or // buttons.innerHTML += '<button>' + letter[i] + '</button>';
+        // or // keyboard.innerHTML += '<button>' + letter[i] + '</button>';
     }
 
 //make inputs by loop
@@ -79,3 +84,5 @@ for ( var i = 0; i < word.length; i++) {
         // console.log(word[i]);
         document.getElementById('input-letters').innerHTML += ' <input value="" id="'+i+'" class="letter-container" disabled>';
     }
+
+    
